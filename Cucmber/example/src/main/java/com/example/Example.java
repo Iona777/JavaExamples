2b55e44a1c852328caec21f02a6efa32c615bba8
Iona777 <greg.macdonald77@gmail.com>
2018-07-16 12:43:27 +0100

    Example cucumber project

---
+++ Cucmber/example/src/main/java/com/example/Example.java
@@ -0,0 +1,6 @@
+package com.example;
+public class Example {
+    public String checkPlay(int playedNumber) {
+        return Integer.toString(playedNumber);
+    }
+}
---
+++ Cucmber/example/src/test/java/com/example/ExampleSteps.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleSteps {
+
+    private Example _target;
+    private String _actualResult;
+    @Given("^I am officiating a FizzBuzz game$")
+    public void I_am_officiating_a_FizzBuzz_game() {
+        _target = new Example();
+    }
+    @When("^the number (\\d+) is played$")
+    public void the_number_is_played(int playedNumber) {
+        _actualResult = _target.checkPlay(playedNumber);
+    }
+    @Then("^I should be told the correct answer is \"([^\"]*)\"$")
+    public void I_should_be_told_the_correct_answer_is(String expectedResult) {
+        assertEquals(expectedResult, _actualResult);
+    }
+
+    @Then("^I do another thing$")
+    public void I_do_another_thing()  {
+        // Express the Regexp above with the code you wish you had
+        System.out.print("I do a thing");
+
+    }
+}
